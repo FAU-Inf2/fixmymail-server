c343d401620305742e9b043983c2c9482e80c0a5
Michael Wiesenbauer <michael.wiesenbauer@studium.fau.de>
2015-05-16 19:41:42 +0200

    minor changes

--- cyrus/init.sh
+++ cyrus/init.sh
@@ -9,17 +9,11 @@ if [ -n "${FIXMYMAILPW}" ]; then
     echo -e "$FIXMYMAILPW\n$FIXMYMAILPW" | passwd fixmymail
 fi
 
-#/usr/sbin/rsyslogd -n &
-#/usr/sbin/sshd -D &
+NAME=cyrmaster
+DAEMON=/usr/sbin/${NAME}
 /etc/init.d/rsyslog start
 /etc/init.d/ssh start
 /etc/init.d/saslauthd start
 /etc/init.d/cyrus-imapd start
-
-#NAME=cyrmaster
-#DAEMON=/usr/sbin/${NAME}
-#PIDFILE="/var/run/${NAME}.pid"
-
-#start-stop-daemon --start --quiet --pidfile $PIDFILE --exec $DAEMON &
 
 wait
--- dovecot/init.sh
+++ dovecot/init.sh
@@ -4,11 +4,6 @@ if [ -n "${FIXMYMAILPW}" ]; then
     echo -e "$FIXMYMAILPW\n$FIXMYMAILPW" | passwd fixmymail
 fi
 
-#/usr/sbin/rsyslogd -n &
-#/usr/sbin/sshd -D &
-#/usr/sbin/dovecot
-#/etc/init.d/rsyslog start
-#/etc/init.d/ssh start
-#/etc/init.d/dovecot start
-svscanboot 
-#wait
+/etc/init.d/rsyslog start
+/etc/init.d/ssh start
+/usr/sbin/dovecot -F
