87986df37519ba64cd5e4f0c4cb3280dae0f03fb
Michael Wiesenbauer <michael.wiesenbauer@studium.fau.de>
2015-05-17 20:36:52 +0200

    fixed courier startup script, process still forked off to background

--- courier/start.sh
+++ courier/start.sh
@@ -10,5 +10,6 @@ service ssh start;
 service postfix start;
 service courier-authdaemon start;
 service courier-imap-ssl start;
-service courier-imap start;
+#service courier-imap start;
+/usr/sbin/courierlogger -name=imapd /usr/sbin/couriertcpd -address=0 -maxprocs=40 -maxperip=20 -nodnslookup -noidentlookup 143 /usr/lib/courier/courier/imaplogin /usr/bin/imapd Maildir
 /bin/bash;
